b2cf33a6d39c12ae4a03af2df65f5a4485a34506
ivanIndjic <ivan123indjic@gmail.com>
2022-02-13 20:25:02 +0100

    Delete method
    Update method

--- flaskapi/post/post.py
+++ flaskapi/post/post.py
@@ -31,3 +31,30 @@ def create_post():
             fields=post_marshmallow.dump(new_post)), 200)
     except ValueError as e:
         raise ValueError(e)
+
+
+@post.route('/delete/<int:id>', methods=['DELETE'])
+def delete_post(id: int):
+    post = Post.query.filter_by(id=id).first()
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+        return make_response('Resource deleted successfully', 200)
+    return make_response('Resource not found', 404)
+
+
+@post.route('/update/<int:id>', methods=['PUT'])
+def update_post(id: int):
+    request_post = request.get_json(force=True)
+    post_updated = Post(**request_post)
+    old_post = Post.query.get(id)
+    if old_post:
+        old_post.name = post_updated.name
+        old_post.created_time = post_updated.created_time
+        old_post.modified_time = post_updated.modified_time
+        old_post.author = post_updated.author
+        old_post.blog = post_updated.blog
+        db.session.commit()
+        post_marshmallow = PostSchema()
+        return make_response(jsonify(fields=post_marshmallow.dump(old_post)), 200)
+    return make_response('Resource not found', 404)

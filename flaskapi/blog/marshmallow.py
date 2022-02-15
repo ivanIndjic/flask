3af45466f0c9a3243f1195501664547c72ee8059
Ivan Indjic <44731503+ivanIndjic@users.noreply.github.com>
2022-02-15 18:24:48 +0100

    Error handling for blog (#5)

--- flaskapi/blog/marshmallow.py
+++ flaskapi/blog/marshmallow.py
@@ -1,8 +1,13 @@
+from marshmallow import fields, validates
 from ..application import ma
 from .blog import Blog
+from ..post.marshmallow import PostSchema
 
+from typing import List
 
 class BlogSchema(ma.SQLAlchemyAutoSchema):
+    title = fields.Str(required=True)
+    posts = fields.List(fields.Nested(PostSchema))
     class Meta:
-        model = Blog
+        fields = ['title', 'posts', 'id']
         exclude = ['id']
